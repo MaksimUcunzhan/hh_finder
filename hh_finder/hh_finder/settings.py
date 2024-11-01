37efb2b3407264ebcc0e974325056828dbe7d339
Maksim Uchunzhyan <maksimucunzan@gmail.com>
2024-11-01 23:35:45 +0300

    создал приложение на сайте, визуал, поисковая строка

--- hh_finder/hh_finder/settings.py
+++ hh_finder/hh_finder/settings.py
@@ -19,6 +19,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'search',
     'main',
     'users',
     'django.contrib.admin',
--- hh_finder/main/urls.py
+++ hh_finder/main/urls.py
@@ -3,6 +3,7 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='home'),
-    path('about', views.about, name='about'),
+    path('search/', include('search.urls', namespace='search')),
     path('users/', include('users.urls'), name='users'),
 ]
+
